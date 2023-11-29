b68d6d6d8cd5ad4e0e660ab93f9485bd61b5206a
Nikita Kuzhlev <n.kuzhlev@idaproject.com>
2023-11-29 23:32:45 +0300

    asd

---
+++ vite.config.js
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite';
+
+import { join, resolve } from 'path';
+import pages, { getRollupInput } from 'vite-mpa';
+import svg from 'vite-sprite-svg';
+
+const rootDir = resolve(__dirname, 'src');
+const pagesDir = resolve(rootDir, 'pages');
+const publicDir = resolve(__dirname, 'public');
+const root = './src';
+const port = 3000;
+
+export default defineConfig(({ command }) => {
+  const isDev = command !== 'build';
+  return {
+    plugins: [
+      svg({
+        baseDir: rootDir,
+        publicDir,
+        isDev: isDev,
+      }),
+      pages(isDev),
+    ],
+    publicDir: publicDir,
+    root: root,
+    build: {
+      outDir: resolve(__dirname, 'dist'),
+      rollupOptions: {
+        input: getRollupInput({ root, rootDir, port, pagesDir }, isDev),
+      },
+      emptyOutDir: true,
+    },
+    server: {
+      port: port,
+      open: '/pages/',
+    },
+    resolve: {
+      alias: {
+        '@components': join(rootDir, 'components'),
+        '@pages': join(rootDir, 'pages'),
+        '@utils': join(rootDir, 'utils'),
+        '@styles': join(rootDir, 'styles'),
+        '@assets': join(rootDir, 'assets'),
+      },
+    },
+  };
+});
